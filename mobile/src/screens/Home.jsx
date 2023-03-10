e6aaff40dc9e008bbe8b8bffc26718d4e6a52f40
safabalo <safaabalha58@gmail.com>
2023-03-10 15:50:03 +0000

    STOR-7 dark mode

--- mobile/src/screens/Home.jsx
+++ mobile/src/screens/Home.jsx
@@ -1,52 +1,63 @@
 import react from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { View, Text , TouchableOpacity,ScrollView, Image, Switch} from 'react-native';
 import Icon from "react-native-vector-icons/Ionicons"
 import Card from '../components/Card'
 import Logo from '../images/logo.png'
+import axios from 'axios';
 
 
 function Home() {
+    const url= "https://newsdata.io/api/1/news?apikey=pub_186594968a793831ccf426e0e67018b60013f&language=fr,en"
+    let [data, setData] = useState([])
     const [isEnabled, setIsEnabled] = useState(false);
-    const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+    const toggleSwitch = () => setIsEnabled(isEnabled => !isEnabled);
+
+    useEffect(() => {
+        axios.get(url)
+        .then((res) => {
+            setData(data = res.data.results)
+            // console.log(data)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    }, [])
     return (
-        <View className="flex-1">
-            {/* <Text className="text-black font-sans text-3xl font-bold mt-4 ml-4">Storial</Text> */}
+        <View className={(isEnabled && "flex-1 bg-[#111]")}>
             <View className="mt-4 ml-4 flex flex-row justify-between">
-                <Text className="text-black font-sans text-3xl font-bold ">Storial</Text>
+                <Text className={(isEnabled && "text-[#eee] font-sans text-3xl font-bold" || "text-black font-sans text-3xl font-bold")}>Storial</Text>
                 <Switch
-                    trackColor={{false: '#767577', true: '#000'}}
-                    thumbColor={isEnabled ? '#ffff' : '#0d0c0c'}
+                    trackColor={{false: '#767577', true: '#767577'}}
+                    thumbColor={isEnabled ? '#fff' : '#0d0c0c'}
                     ios_backgroundColor="#3e3e3e"
                     onValueChange={toggleSwitch}
                     value={isEnabled}
                 />
             </View>
-            <View className="flex-row items-center gap-x-1 my-8">
+            <View className="flex-row items-center gap-x-1 my-8" >
                 <Icon name='chevron-back-sharp' size={30} color="#b3afaf"/>
-                <TouchableOpacity className="bg-black text-white w-28 h-10 rounded-lg flex flex-row justify-center gap-x-1 items-center">
+                <TouchableOpacity className={(isEnabled && "bg-slate-700 w-28 h-10 rounded-lg flex flex-row justify-center gap-x-1 items-center" || "bg-black w-28 h-10 rounded-lg flex flex-row justify-center gap-x-1 items-center")}>
                     <Icon name="md-football" size={20} color='#fff' />
                     <Text className="text-white font-semibold">Sport</Text>
                 </TouchableOpacity>
-                <TouchableOpacity className="bg-black w-28 h-10 rounded-lg flex flex-row justify-center gap-x-1 items-center">
+                <TouchableOpacity className={(isEnabled && "bg-slate-700 w-28 h-10 rounded-lg flex flex-row justify-center gap-x-1 items-center" || "bg-black w-28 h-10 rounded-lg flex flex-row justify-center gap-x-1 items-center")}>
                     <Icon name='md-musical-notes-outline' size={20} color="#fff"/>
                     <Text className="text-white font-semibold">Musique</Text>
                 </TouchableOpacity>
-                <TouchableOpacity className="bg-black w-28 h-10 rounded-lg flex flex-row justify-center gap-x-1 items-center">
+                <TouchableOpacity className={(isEnabled && "bg-slate-700 w-28 h-10 rounded-lg flex flex-row justify-center gap-x-1 items-center" || "bg-black w-28 h-10 rounded-lg flex flex-row justify-center gap-x-1 items-center")}>
                     <Icon name='ios-briefcase-outline' size={20} color="#fff"/>
                     <Text className="text-white font-semibold">Politique</Text>
                 </TouchableOpacity>
                 <Icon name='chevron-forward-sharp' size={30} color="#b3afaf"/>
             </View>
+            
             <ScrollView className="overflow-y-scroll h-screen" style={{ height: '80%' }}>
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
+            {data.map((item) => (
+                // <Card isEnabled={isEnabled} title={item.title} description={item.description}/>
+            ))}
             </ScrollView>
+            
         </View>
     )
 }
